03c677b79f9303b19883a280ddc8b0f18b511823
Magnus Nording <120574513+MagNording@users.noreply.github.com>
2023-09-25 13:27:24 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,120 @@
+// Magnus Nording, magnus.nording@iths.se
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+
+    private static ArrayList<Product> allProducts = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        boolean exitMenu = false;
+
+        System.out.println("FRUKT OCH GRÖNT\nProgrammet startas.\n");
+
+        do {
+            displayMenu();
+
+            try {
+                int menuChoice = input.nextInt();
+                input.nextLine();
+                switch (menuChoice) {
+                    case 0 -> exitMenu = true;
+                    case 1 -> addNewProduct();
+                    case 2 -> removeProduct();
+                    case 3 -> searchProduct();
+                }
+            } catch (InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Felaktig inmatning, försök igen.");
+            }
+            System.out.println();
+        } while (!exitMenu);
+
+        System.out.println("Tack, programmet avslutas.");
+    }
+
+    public static void displayMenu() {
+        // Skapa en array
+        String[] menu = {
+                "0. Avsluta programmet.",
+                "1. Lägg till en produkt.",
+                "2. Ta bort en produkt.",
+                "3. Sök produkt.",
+                "Ange ditt menyval: "};
+        for (String choice : menu) {
+            System.out.println(choice);
+        }
+    }
+
+    public static void addNewProduct() {
+        boolean validInput = true;
+        while (validInput) {
+            try {
+                System.out.print("Ange varans namn: ");
+                String nameInput = input.nextLine();
+                System.out.print("Ange kilopris eller styckpris: ");
+                double priceInput = input.nextDouble();
+                input.nextLine();
+                System.out.print("Ange varugrupp: ");
+                String categoryInput = input.nextLine();
+                Product product = new Product(nameInput, priceInput, categoryInput);
+                allProducts.add(product);
+                validInput = false;
+            } catch (InputMismatchException ime) {
+                input.nextLine();
+                System.out.println("Felaktig inmatning, försök igen.\n" +
+                        "Tänk på att använda kommatecken i decimaltal.");
+            }
+        }
+    }
+
+    public static void removeProduct() {
+        boolean found = false;
+        System.out.println("Ange produkten du vill ta bort: ");
+        searchProduct();
+            /*String productToRemove = input.nextLine();
+
+            for (int i = 0; i < allProducts.size(); i++) {
+            Product product = allProducts.get(i);
+            // om produktnamn eller varugrupp, små bokstäver påminner om inputen, små bokstäver
+            if (product.getName().toLowerCase().contains(productToRemove.toLowerCase()) ||
+                    product.getProductGroup().toLowerCase().contains(productToRemove.toLowerCase()))*/ {
+                System.out.println("Är du säker på att du vill ta bort: " + product.getName() + " (j/n)");
+                String answer = input.nextLine();
+                if (answer.equalsIgnoreCase("j")) {
+                    allProducts.remove(i);
+                    found = true;
+                } else if (answer.equalsIgnoreCase("n")) {
+                    // Användaren vill gå tillbaka till menyn
+                    return;
+                }
+            }
+        //}
+        if (found) {
+            System.out.println(searchProduct() + " har tagits bort.");
+        } else {
+            System.out.println("Produkten hittades inte.");
+        }
+    }
+
+    public static Product searchProduct() {
+        if (allProducts.isEmpty()) {
+            System.out.println("Produktlistan är tom.");
+        } else {
+            System.out.println("Ange sökterm: ");
+            String searchTerm = input.nextLine();
+            //boolean found = false;
+            for (int i = 0; i < allProducts.size(); i++) {
+                Product product = allProducts.get(i);
+                if (product.getName().toLowerCase().contains(searchTerm.toLowerCase()) ||
+                        product.getProductGroup().toLowerCase().contains(searchTerm.toLowerCase())) {
+                    System.out.println(product);
+                    return product;
+                }
+            }
+        }
+    }
+}
