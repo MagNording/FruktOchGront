2b51147a18827fe7e8f6ecf0b6b117cd64bd4584
Magnus Nording <120574513+MagNording@users.noreply.github.com>
2023-09-25 13:30:21 +0200

    Add files via upload
    Another version

---
+++ ProductType2.java
@@ -0,0 +1,51 @@
+// magnus.nording@iths.se
+public class Product {
+    private String name;
+    private double price;
+    private String productGroup;
+    
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name, double price, String productGroup) {
+        this.name = name;
+        this.price = price;
+        this.productGroup = productGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "Namn: %s, pris: %.2f kr/kg, varugrupp: %s"
+                .formatted(name, price, productGroup);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductGroup() {
+        return this.productGroup;
+    }
+
+    public void setProductGroup(String productGroup) {
+        this.productGroup = productGroup;
+    }
+}
