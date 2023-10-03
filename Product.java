d20532a45e6f0637ea412d388082efae98b87a85
Magnus Nording <120574513+MagNording@users.noreply.github.com>
2023-10-03 16:23:06 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -2,7 +2,7 @@
 public class Product {
     private String name;
     private double price;
-    private String productGroup;
+    private String[] productGroup;
     private boolean isWeightPrice;
 
     public Product(String name) {
@@ -14,20 +14,25 @@ public class Product {
         this.price = price;
     }
 
-    public Product(String name, double price, String productGroup, boolean isWeightPrice) {
+    public Product(String name, double price, String[] productGroup, boolean isWeightPrice) {
         this.name = name;
         this.price = price;
         this.productGroup = productGroup;
         this.isWeightPrice = isWeightPrice;
+
     }
 
     @Override
-    public String toString() { 
-        if (isWeightPrice) {
-            return "Produkt: Vara: %s Pris: %.2f Varugrupp: %s".
-                    formatted(name, price, productGroup);
-        } return "Produkt: Vara: %s Pris: %.2f Varugrupp: %s".
-                formatted(name, price, productGroup);
+    public String toString() {
+        String priceType = isWeightPrice ? "Pris/kg" : "Pris/st";
+
+        String productGroupStr = "";
+        if (productGroup != null && productGroup.length > 0) {
+            productGroupStr = String.join(", ", productGroup);
+        }
+
+        return String.format("Produkt:%s %s: %.2f Varugrupp: %s",
+                name, priceType, price, productGroupStr);
     }
 
     public String getName() {
@@ -46,11 +51,11 @@ public class Product {
         this.price = price;
     }
 
-    public String getProductGroup() {
-        return this.productGroup;
+    public String[] getProductGroup() {
+        return productGroup;
     }
 
-    public void setProductGroup(String productGroup) {
+    public void setProductGroup(String[] productGroup) {
         this.productGroup = productGroup;
     }
 
@@ -60,5 +65,13 @@ public class Product {
 
     public void setWeightPrice(boolean weightPrice) {
         isWeightPrice = weightPrice;
+    }
+
+    public boolean getIsWeightPrice() {
+        return this.isWeightPrice;
+    }
+
+    public void setIsWeightPrice(boolean weightPrice) {
+        this.isWeightPrice = weightPrice;
     }
 }
